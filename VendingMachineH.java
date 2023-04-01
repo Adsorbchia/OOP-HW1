2b272a30bc726894bd8c091d70c33a87012dbebe
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2023-04-01 21:39:43 +0400

    feat:add new class VMH

---
+++ VendingMachineH.java
@@ -0,0 +1,7 @@
+package org.ex.seminar1.hw1;
+
+import java.util.ArrayList;
+
+public interface VendingMachineH {
+    ArrayList getProduct(String searchName, Integer searchVolume,Integer searchTemperature);
+}
